7258a2180f859ec73cb0384719befbda50ef0615
JahanSimran <jahansimran244@gmail.com>
2024-07-11 11:29:41 +0530

    first commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# tagline-infotech-practicle
+A demo app 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name" = () => {
+  return <div> git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"</div>;
+};
+
+export default
+  git config--global user.email "you@example.com"
+  git config --global user.name "Your Name"
